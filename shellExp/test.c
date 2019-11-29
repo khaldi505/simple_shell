1f1e4bbf8fca614c0e337e1225fdd9e9236bf116
Atef Mechken <habetlbrika69@gmail.com>
2019-11-29 23:15:38 +0100

    Betty styling main testing file

--- shellExp/test.c
+++ shellExp/test.c
@@ -1,14 +1,15 @@
 #include "shellHeader.h"
 
-int main(void) {
-  char *command;
-  size_t bus = 32;
-  int ss;
-  command = (char *)malloc(sizeof(char) * bus);
-  ss = getline(&command, &bus, stdin);
-  printf("%s\n", command);
-  if (isatty(STDIN_FILENO) == 1)
-  printf("lol\n");
+int main(void)
+{
+char *command;
+size_t bus = 32;
+int ss;
+command = (char *)malloc(sizeof(char) * bus);
+ss = getline(&command, &bus, stdin);
+printf("%s\n", command);
+if (isatty(STDIN_FILENO) == 1)
+printf("lol\n");
 
-  printf("%s\n", command);
+printf("%s\n", command);
 }
